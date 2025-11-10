51ef3d9582a1efec9bc18a816eef904708057937
Adewale Akangbe <adewaleakangbe14@gmail.com>
2025-11-10 10:08:45 +0100

    Add dbt project and seeds

--- dbt/nomba_analytics/models/marts/dim_customer.sql
+++ dbt/nomba_analytics/models/marts/dim_customer.sql
@@ -3,6 +3,6 @@ with users as (
     row_number() over (partition by user_id order by updated_at desc nulls last) rn
   from {{ ref('stg_mongo_user') }}
 )
-select user_id, first_name, last_name, occupation, state, updated_at
+select source_id, first_name, last_name, occupation, state, updated_at
 from users
 where rn = 1
